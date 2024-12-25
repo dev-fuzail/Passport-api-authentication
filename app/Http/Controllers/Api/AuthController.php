e21bdb8798d026e97fc6fcbf7117a16a6134f3e0
Muhammad Hashir Shaikh <42890607+dev-fuzail@users.noreply.github.com>
2024-12-25 23:51:52 +0500

    Initial commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        print_r($request->all());
+    }
+}
